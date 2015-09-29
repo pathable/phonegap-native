ccbb93e6eaf7c6498da2d742dff1ad12d4121f04
Administrator <bakbang@yahoo.dom>
2015-09-29 02:23:43 +0200

    Intialize project to build phonegap

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,61 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function onLoadHandler() {
+    $('#loading-container').addClass('hidden');
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+document.addEventListener("deviceready", init, false);
 
-        console.log('Received Event: ' + id);
-    }
-};
+function init() {
+    var $window = $(window);
+    var $app = $('#application');
+    var app = $app[0].contentWindow;
+    var a = document.createElement('a');   
+
+    $window.on("message", function (e) {
+        var data = e.originalEvent.data;
+        switch (data.route) {
+            case 'reload':
+                $window.scrollTop(0)
+                app.location.reload(true);
+                break;
+            case 'scrollTop':
+                $window.scrollTop(0);
+                break;
+            case 'update':
+                break;
+            case 'open-url':
+                var url = data.url;
+                break;
+            case 'push-registration':
+                break;
+            case 'push-registration/badge-clear':
+                break;
+            case 'bar-codes/new':
+                if(cordova){
+                    cordova.plugins.barcodeScanner.scan(
+                        function (result) {
+                            alert(result.text);
+                            var value = btoa(result.text);
+                            var organization_id = data.organization_id; // to create leads when user scans org
+                            var user_id = data.user_id; // to create lead when organization scans user
+                            var group_id = data.group_id; // communities or meetings, check in applied to memberships
+
+                            if (organization_id) {
+                              route = 'organizations/' + organization_id + '/bar-codes/' + value;
+                            } else if (user_id) {
+                              route = 'users/' + user_id + '/bar-codes/' + value;
+                            } else if (group_id) {
+                              route = 'groups/' + group_id + '/bar-codes/' + value;
+                            } else {
+                              route = 'bar-codes/' + value
+                            };
+                            app.postMessage({route: route}, '*');
+                        },
+                        function (error) {
+                            alert("Scanning failed: " + error);
+                        }
+                    );
+                }
+                break;
+        };
+    });
+}
