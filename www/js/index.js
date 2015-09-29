b4f7e780c93d7397e4daf586725194b7034e2ffc
Administrator <bakbang@yahoo.dom>
2015-09-29 04:58:10 +0200

    Added barcode plugin (I've built locally and it's working well.)

--- www/js/index.js
+++ www/js/index.js
@@ -1,61 +1,91 @@
-function onLoadHandler() {
-    $('#loading-container').addClass('hidden');
-}
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
 
-document.addEventListener("deviceready", init, false);
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        app.loadPage();
+    },
 
-function init() {
-    var $window = $(window);
-    var $app = $('#application');
-    var app = $app[0].contentWindow;
-    var a = document.createElement('a');   
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        console.log('Received Event: ' + id);
+    },
 
-    $window.on("message", function (e) {
-        var data = e.originalEvent.data;
-        switch (data.route) {
-            case 'reload':
-                $window.scrollTop(0)
-                app.location.reload(true);
-                break;
-            case 'scrollTop':
-                $window.scrollTop(0);
-                break;
-            case 'update':
-                break;
-            case 'open-url':
-                var url = data.url;
-                break;
-            case 'push-registration':
-                break;
-            case 'push-registration/badge-clear':
-                break;
-            case 'bar-codes/new':
-                if(cordova){
-                    cordova.plugins.barcodeScanner.scan(
-                        function (result) {
+    encode: function() {
+
+    },
+    
+    loadPage: function() {
+        var $window = $(window);
+        var $app = $('#application');
+        var app = $app[0].contentWindow;
+        var a = document.createElement('a');   
+
+        $window.on("message", function (e) {
+            var data = e.originalEvent.data;
+            switch (data.route) {
+                case 'reload':
+                    $window.scrollTop(0)
+                    app.location.reload(true);
+                    break;
+                case 'scrollTop':
+                    $window.scrollTop(0);
+                    break;
+                case 'update':
+                    break;
+                case 'open-url':
+                    var url = data.url;
+                    break;
+                case 'push-registration':
+                    break;
+                case 'push-registration/badge-clear':
+                    break;
+                case 'bar-codes/new':
+                    alert('scan');
+                    if(cordova){
+                        cordova.plugins.barcodeScanner.scan(function (result) { 
+
+                            alert("We got a barcode\n" + 
+                            "Result: " + result.text + "\n" + 
+                            "Format: " + result.format + "\n" + 
+                            "Cancelled: " + result.cancelled);  
+
+                           console.log("Scanner result: \n" +
+                                "text: " + result.text + "\n" +
+                                "format: " + result.format + "\n" +
+                                "cancelled: " + result.cancelled + "\n");
                             alert(result.text);
-                            var value = btoa(result.text);
-                            var organization_id = data.organization_id; // to create leads when user scans org
-                            var user_id = data.user_id; // to create lead when organization scans user
-                            var group_id = data.group_id; // communities or meetings, check in applied to memberships
+                            console.log(result);
+                            /*
+                            if (args.format == "QR_CODE") {
+                                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                            }
+                            */
 
-                            if (organization_id) {
-                              route = 'organizations/' + organization_id + '/bar-codes/' + value;
-                            } else if (user_id) {
-                              route = 'users/' + user_id + '/bar-codes/' + value;
-                            } else if (group_id) {
-                              route = 'groups/' + group_id + '/bar-codes/' + value;
-                            } else {
-                              route = 'bar-codes/' + value
-                            };
-                            app.postMessage({route: route}, '*');
-                        },
-                        function (error) {
-                            alert("Scanning failed: " + error);
-                        }
-                    );
-                }
-                break;
-        };
-    });
-}
+                        }, function (error) { 
+                            console.log("Scanning failed: ", error); 
+                        } );
+                    }
+                    break;
+            };
+        });
+    },
+    onLoadHandler: function () {
+        $('#loading-container').addClass('hidden');
+    }
+
+};
