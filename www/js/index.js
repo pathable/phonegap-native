c529c71ba00d242b232f1029652de0f2defb5a3a
Administrator <bakbang@yahoo.dom>
2015-10-02 19:19:04 +0200

    Added themableInappBrowser plugin

--- www/js/index.js
+++ www/js/index.js
@@ -84,8 +84,7 @@ var app = {
                     break;
                 case 'open-url':
                     var url = data.url;
-                    var ref = cordova.InAppBrowser.open(url, '_blank', 'location=yes');
-                    window.open = cordova.InAppBrowser.open;
+                    app.openUrl(url);
                     break;
                 case 'push-registration':
                     
@@ -127,6 +126,75 @@ var app = {
                 break;
             };
         });
+    },
+    openUrl : function(url){
+        cordova.ThemeableBrowser.open(url, '_blank', {
+        statusbar: {
+            color: '#ffffffff'
+        },
+        toolbar: {
+            height: 44,
+            color: '#f0f0f0ff'
+        },
+        title: {
+            color: '#003264ff',
+            showPageTitle: true
+        },
+        backButton: {
+            image: 'back',
+            imagePressed: 'back_pressed',
+            align: 'left',
+            event: 'backPressed'
+        },
+        forwardButton: {
+            image: 'forward',
+            imagePressed: 'forward_pressed',
+            align: 'left',
+            event: 'forwardPressed'
+        },
+        closeButton: {
+            image: 'close',
+            imagePressed: 'close_pressed',
+            align: 'left',
+            event: 'closePressed'
+        },
+        customButtons: [
+            {
+                image: 'share',
+                imagePressed: 'share_pressed',
+                align: 'right',
+                event: 'sharePressed'
+            }
+        ],
+        menu: {
+            image: 'menu',
+            imagePressed: 'menu_pressed',
+            title: 'Test',
+            cancel: 'Cancel',
+            align: 'right',
+            items: [
+                {
+                    event: 'helloPressed',
+                    label: 'Hello World!'
+                },
+                {
+                    event: 'testPressed',
+                    label: 'Test!'
+                }
+            ]
+        },
+        backButtonCanClose: true
+    }).addEventListener('backPressed', function(e) {
+        alert('back pressed');
+    }).addEventListener('helloPressed', function(e) {
+        alert('hello pressed');
+    }).addEventListener('sharePressed', function(e) {
+        alert(e.url);
+    }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function(e) {
+        console.error(e.message);
+    }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function(e) {
+        console.log(e.message);
+    });
     },
     onLoadHandler: function () {
         $('#loading-container').addClass('hidden');
