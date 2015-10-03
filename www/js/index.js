74ed8150a7146b56e6e69f91b900f0cfe3de2d7c
Administrator <bakbang@yahoo.dom>
2015-10-03 14:28:03 +0200

    iOS splash screen load first

--- www/js/index.js
+++ www/js/index.js
@@ -115,8 +115,7 @@ var app = {
                             route = 'groups/' + group_id + '/bar-codes/' + value;
                         } else {
                             route = 'bar-codes/' + value
-                        }
-                        ;
+                        };
 
                         app.postMessage({route: route}, '*');
 
@@ -127,76 +126,6 @@ var app = {
             }
             ;
         });
-    },
-    openUrl: function (url) {
-        cordova.ThemeableBrowser.open(url, '_blank', {
-            statusbar: {
-                color: '#ffffffff'
-            },
-            toolbar: {
-                height: 44,
-                color: '#f0f0f0ff'
-            },
-            title: {
-                color: '#003264ff',
-                showPageTitle: true
-            },
-            backButton: {
-                image: 'back',
-                imagePressed: 'back_pressed',
-                align: 'left',
-                event: 'backPressed'
-            },
-            forwardButton: {
-                image: 'forward',
-                imagePressed: 'forward_pressed',
-                align: 'left',
-                event: 'forwardPressed'
-            },
-            closeButton: {
-                image: 'close',
-                imagePressed: 'close_pressed',
-                align: 'left',
-                event: 'closePressed'
-            },
-            customButtons: [
-                {
-                    image: 'share',
-                    imagePressed: 'share_pressed',
-                    align: 'right',
-                    event: 'sharePressed'
-                }
-            ],
-            menu: {
-                image: 'menu',
-                imagePressed: 'menu_pressed',
-                title: 'Test',
-                cancel: 'Cancel',
-                align: 'right',
-                items: [
-                    {
-                        event: 'helloPressed',
-                        label: 'Hello World!'
-                    },
-                    {
-                        event: 'testPressed',
-                        label: 'Test!'
-                    }
-                ]
-            },
-            backButtonCanClose: true
-        }).addEventListener('backPressed', function (e) {
-            alert('back pressed');
-        }).addEventListener('helloPressed', function (e) {
-            alert('hello pressed');
-        }).addEventListener('sharePressed', function (e) {
-            alert(e.url);
-        }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function (e) {
-            console.error(e.message);
-        }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function (e) {
-            console.log(e.message);
-        });
-        window.open = cordova.ThemeableBrowser.open();
     },
     onLoadHandler: function () {
         $('#loading-container').addClass('hidden');
